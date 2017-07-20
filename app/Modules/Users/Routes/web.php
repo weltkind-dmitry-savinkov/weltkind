3874731ee7870e07e81802ee3b6a215412147f7c
weltkind-dmitry-savinkov <dsavinkov@weltkind.com>
2017-07-20 17:24:27 +0600

    admin fix

--- app/Modules/Users/Routes/web.php
+++ app/Modules/Users/Routes/web.php
@@ -2,14 +2,12 @@
 
 
 Route::group(['prefix' => config('cms.uri')], function() {
-
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\LoginController@login');
-    Route::get('logout', 'Auth\LoginController@logout');
-
     Route::resource('users', 'Admin\IndexController');
 
     #Route::get('news', 'App\Modules\News\Http\Controllers\Index@index');
 
 
 });
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::get('logout', 'Auth\LoginController@logout');
--- app/Modules/Users/Users/Config/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-return[
-    'title'         => trans('users::admin.title'),
-    'localization'  => true,
-    'in_roles'      => 1
-];
